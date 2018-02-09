8bed24b3e612b9005b3fd4d9feeaa07dadfad101
Adrian Sims <asims@parkeon.com>
2018-02-09 09:35:35 +0000

    Add project files.

---
+++ Watcher1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watcher1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                int count = 0;
+                var watcher = new FileSystemWatcher("\\\\engcdtrans\\fromjenkins\\sybil\\rp");
+                watcher.Filter = "*";
+//                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                watcher.Created += (s, e) =>
+                {
+                    Console.WriteLine("Creation");
+                };
+                watcher.Changed += (s, e) =>
+                {
+                    Console.WriteLine($"{count++} Changed");
+                };
+                watcher.EnableRaisingEvents = true;
+                System.Threading.Thread.Sleep(100000000);
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Watcher1/Program.cs
+git add Watcher1/App.config
+git add Watcher1/Properties/AssemblyInfo.cs
+git add Watcher1/Watcher1.csproj
+git add Watcher1.sln
+git add .gitignore
